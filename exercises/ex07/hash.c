f7bdcd7b3abbbd231201d4cd84ff041b8526b39f
Nathaniel Tan <nathaniel98.tan@gmail.com>
2020-04-02 23:41:17 -0400

    did basic assignment

--- exercises/ex07/hash.c
+++ exercises/ex07/hash.c
@@ -178,8 +178,10 @@ int hash_hashable(Hashable *hashable)
 */
 int equal_int (void *ip, void *jp)
 {
-    // FILL THIS IN!
-    return 0;
+    int *i = (int *)ip;
+    int *j = (int *)jp;
+   
+    return *i == *j;
 }
 
 
@@ -192,8 +194,9 @@ int equal_int (void *ip, void *jp)
 */
 int equal_string (void *s1, void *s2)
 {
-    // FILL THIS IN!
-    return 0;
+    char *st1 = (char *)s1;
+    char *st2 = (char *)s2;
+    return strcmp(st1, st2) == 0;
 }
 
 
@@ -207,8 +210,10 @@ int equal_string (void *s1, void *s2)
 */
 int equal_hashable(Hashable *h1, Hashable *h2)
 {
-    // FILL THIS IN!
-    return 0;
+    int (*equal) (void *, void *);   // FILL THIS IN!
+    equal = h1->equal;
+
+    return equal(h1->key,h2->key);
 }
 
 
@@ -296,7 +301,16 @@ Node *prepend(Hashable *key, Value *value, Node *rest)
 /* Looks up a key and returns the corresponding value, or NULL */
 Value *list_lookup(Node *list, Hashable *key)
 {
-    // FILL THIS IN!
+    Node *current = list;
+    while (current != NULL)
+    {
+        if(equal_hashable(current->key,key))
+        {
+            return current->value;
+        }
+
+        current = current->next;
+    }
     return NULL;
 }
 
@@ -307,6 +321,7 @@ typedef struct map {
     int n;
     Node **lists;
 } Map;
+
 
 
 /* Makes a Map with n lists. */
@@ -341,15 +356,16 @@ void print_map(Map *map)
 /* Adds a key-value pair to a map. */
 void map_add(Map *map, Hashable *key, Value *value)
 {
-    // FILL THIS IN!
+    int mapIndex = hash_hashable(key) % map->n;
+    map->lists[mapIndex] = prepend(key, value, map->lists[mapIndex]);
 }
 
 
 /* Looks up a key and returns the corresponding value, or NULL. */
 Value *map_lookup(Map *map, Hashable *key)
 {
-    // FILL THIS IN!
-    return NULL;
+    int mapIndex = hash_hashable(key) % map->n;
+    return list_lookup(map->lists[mapIndex], key);
 }
 
 
